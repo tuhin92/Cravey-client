34fdcc1f7449f30079bc3584c800f768905367d6
tuhin92 <tuhinalmamun71m@gmail.com>
2025-02-06 17:49:04 +0600

    set min height on home and about page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='min-h-screen'>
             <h2 className='text-4xl font-bold text-red-500'>This is home page</h2>
         </div>
     );
