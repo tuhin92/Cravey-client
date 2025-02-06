69d22233fa120b77dd6c2e075342a8e981133a0d
tuhin92 <tuhinalmamun71m@gmail.com>
2025-02-06 16:31:21 +0600

    setup basic react project with react router,tailwind css and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-amber-500'>This is home page</h2>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
+import './index.css'
+import App from './App.jsx'
+
+
+// Create a root and render the App component
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
