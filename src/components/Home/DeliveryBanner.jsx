2fe34a215c68bcb033ff70945b9fcf2edddb0bed
tuhin92 <tuhinalmamun71m@gmail.com>
2025-02-06 20:01:01 +0600

    added delivery banner

---
+++ src/components/Home/DeliveryBanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DeliveryBanner = () => {
+  return (
+    <div>
+      <img
+        className="h-auto md:h-[500px] w-screen"
+        src="https://i.ibb.co.com/Qm1nbRn/photo-2024-09-19-11-30-21.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default DeliveryBanner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Banner from "../components/Home/Banner";
 import BestDeliveredCategory from "../components/Home/BestDeliveredCategory";
+import DeliveryBanner from "../components/Home/DeliveryBanner";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       </Helmet>
       <Banner/>
       <BestDeliveredCategory/>
+      <DeliveryBanner/>
     </div>
   );
 };
